da9849c1022538dc02c10c80547aa9c1c024221b
MassDeface <aneshp0258@gmail.com>
2020-12-27 19:58:57 +0100

    done

--- front/src/index.js
+++ front/src/index.js
@@ -1,18 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Login from "./Components/Login/Login";
-import reportWebVitals from "./reportWebVitals";
-import Profil from "./Components/Profil/Profil.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Profil />
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.render(<App />,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
