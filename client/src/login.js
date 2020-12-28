8f3c2ea9803360185e9e0aa229aaf82c0c76608e
melek-houidi <71493942+melek-houidi@users.noreply.github.com>
2020-12-28 11:10:54 +0100

    melek

--- client/src/login.js
+++ client/src/login.js
@@ -1,39 +1,58 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
+import "../src/login.css";
 export default class login extends Component {
-    constructor(props){
-    super(props)
-    this.state ={
-        email:'',
-        password:''
-    }
-    }
-    //send data of login to db 
-    async login(){
-       await axios.post('/login',{
-            email : this.state.email,
-            password : this.state.password
-        }).then(function (res) {
-            if(res.data.status === 'successLogg'){
-                console.log(res);
-                document.cookie = `username=${res.data.useremail},${res.data.username}`;
-                window.location.replace("/form");
-            }
-          })
-    }
-    render() {
-        return (
-            <div className="logs">
-                <h1>login</h1>
-                <hr></hr>
-                <input type='email' placeholder="enter your Email please" onChange={(e)=>this.setState({email: e.target.value})}></input>
-                <hr></hr>
-                <input type='password' placeholder="enter your password please" onChange={(e)=>this.setState({password: e.target.value})}></input>
-                <hr></hr>
-                <button type='submit' onClick={this.login.bind(this)}>submit</button>
-                <hr></hr>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+  //send data of login to db
+  async login() {
+    await axios
+      .post("/login", {
+        email: this.state.email,
+        password: this.state.password,
+      })
+      .then(function (res) {
+        if (res.data.status === "successLogg") {
+          console.log(res);
+          document.cookie = `username=${res.data.useremail},${res.data.username}`;
+          window.location.replace("/form");
+        }
+      });
+  }
+  render() {
+    return (
+      <div className="logs">
+        <h1>login</h1>
+        <hr></hr>
+        <div className="textbox">
+          <i className="fas fa-user"></i>
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            onChange={(e) => this.setState({ email: e.target.value })}
+          ></input>
+        </div>
+        <hr></hr>
+        <div className="textbox">
+          <i className="fas fa-lock"></i>
+          <input
+            type="password"
+            placeholder="Enter Your Password"
+            onChange={(e) => this.setState({ password: e.target.value })}
+          ></input>
+        </div>
+        <hr></hr>
+        <button className="btn" type="submit" onClick={this.login.bind(this)}>
+          Submit
+        </button>
+        <hr></hr>
+      </div>
+    );
+  }
 }
 //this code made by anas
